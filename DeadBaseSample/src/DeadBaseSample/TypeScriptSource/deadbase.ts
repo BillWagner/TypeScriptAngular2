6666ca0c1bcca1103d30efc6cac238f7ee2a1fb7
Bill Wagner <bill@thebillwagner.com>
2015-12-14 11:54:46 -0500

    First Basic Sample.
    This one runs the typescript compiler in the browser.
    Dependencies are loaded from the web directly.

---
+++ DeadBaseSample/src/DeadBaseSample/TypeScriptSource/deadbase.ts
@@ -0,0 +1,12 @@
+﻿import { bootstrap, Component } from 'angular2/angular2';
+
+@Component({
+    selector: 'deadbase-app',
+    template: '<h1>This is loaded from TypeScript</h1>'
+})
+class DeadBaseAppComponent {
+}
+
+bootstrap(DeadBaseAppComponent);
+
+
