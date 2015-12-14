0d9c980f8266f86dfabe0cb4317385dec7fb9aac
Bill Wagner <bill@thebillwagner.com>
2015-12-14 14:59:50 -0500

    Include the NgIf directive

--- DeadBaseSample/src/DeadBaseSample/TypeScriptSource/deadbase.ts
+++ DeadBaseSample/src/DeadBaseSample/TypeScriptSource/deadbase.ts
@@ -1,4 +1,4 @@
-﻿import { bootstrap, Component, FORM_DIRECTIVES, NgFor } from 'angular2/angular2';
+﻿import { bootstrap, Component, FORM_DIRECTIVES, NgFor, NgIf } from 'angular2/angular2';
 
 class ConcertSet {
     date: string;
@@ -45,7 +45,7 @@ var AllConcerts: ConcertSet[] = [
             <div><label>set: </label><input [(ng-model)]="selectedConcert.set" type="number"></div>
         </div>
     `,
-    directives: [FORM_DIRECTIVES, NgFor],
+    directives: [FORM_DIRECTIVES, NgFor, NgIf],
     styles: [`
       .concerts {list-style-type: none; margin-left: 1em; padding: 0; width: 30em;}
       .concerts li { cursor: pointer; position: relative; left: 0; transition: all 0.2s ease; }
