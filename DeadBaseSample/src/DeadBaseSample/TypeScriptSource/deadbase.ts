72d3f7de35b2639e7b1a1f0ceba97347850b3677
Bill Wagner <bill@thebillwagner.com>
2015-12-14 13:32:27 -0500

    Create class for a concert set

--- DeadBaseSample/src/DeadBaseSample/TypeScriptSource/deadbase.ts
+++ DeadBaseSample/src/DeadBaseSample/TypeScriptSource/deadbase.ts
@@ -1,12 +1,23 @@
 ﻿import { bootstrap, Component } from 'angular2/angular2';
 
+class ConcertSet {
+    date: Date;
+    venue: string;
+    set: number;
+}
+
+
 @Component({
     selector: 'deadbase-app',
-    template: '<h1>{{title}}</h1><h2>{{venue}} Details!</h2>'
+    template: '<h1>{{title}}</h1><h2>{{concert.date}} -- {{concert.venue}} Details!</h2>'
 })
 class DeadBaseAppComponent {
     public title = "Deadbase - Grateful Dead Concert Archive";
-    public venue = "Filmore West";
+    public concert: ConcertSet = {
+        date: new Date(1971, 7, 2),
+        venue: "Filmore West",
+        set: 2
+    }
 }
 
 bootstrap(DeadBaseAppComponent);
