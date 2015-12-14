511682bd3384b17658190c9640f8e6d4d93c36dd
Bill Wagner <bill@thebillwagner.com>
2015-12-14 11:04:59 -0500

    Create gulp file for html delivery

---
+++ DeadBaseSample/src/DeadBaseSample/gulpfile.js
@@ -0,0 +1,13 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+
+gulp.task('install-html', function () {
+    gulp.src('HtmlSource/*')
+    .pipe(gulp.dest('wwwroot'));
+});
+
+gulp.task('default', ['install-html']);
