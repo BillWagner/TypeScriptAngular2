d7867cf5f7f821eb424b2e6a32c98ddeca2d808a
Bill Wagner <bill@thebillwagner.com>
2015-12-14 11:15:16 -0500

    add all the node dependencies

--- DeadBaseSample/src/DeadBaseSample/gulpfile.js
+++ DeadBaseSample/src/DeadBaseSample/gulpfile.js
@@ -1,4 +1,5 @@
-﻿/*
+﻿/// <binding AfterBuild='install-html' />
+/*
 This file in the main entry point for defining Gulp tasks and using Gulp plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 */
