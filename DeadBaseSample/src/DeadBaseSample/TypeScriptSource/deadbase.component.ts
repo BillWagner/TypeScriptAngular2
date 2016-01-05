4af4393bb136781d37b230ab6cc5a0733d565184
Bill Wagner <bill@thebillwagner.com>
2016-01-05 17:41:11 -0500

    update for Beta 0

--- DeadBaseSample/src/DeadBaseSample/TypeScriptSource/deadbase.component.ts
+++ DeadBaseSample/src/DeadBaseSample/TypeScriptSource/deadbase.component.ts
@@ -20,27 +20,4 @@ export class DeadBaseAppComponent {
     }
 }
 
-var AllConcerts: ConcertSet[] = [
-    { date: "1971-07-02", venue: "Filmore West", set: 1 },
-    { date: "1971-07-02", venue: "Filmore West", set: 2 },
-    { date: "1971-12-14", venue: "Hill Auditorium", set: 1 },
-    { date: "1971-12-14", venue: "Hill Auditorium", set: 2 },
-    { date: "1978-12-31", venue: "The Winterland Auditorium", set: 1 },
-    { date: "1978-12-31", venue: "The Winterland Auditorium", set: 2 },
-    { date: "1978-12-31", venue: "The Winterland Auditorium", set: 3 },
-    { date: "1989-04-05", venue: "Crisler Arena", set: 1 },
-    { date: "1989-04-05", venue: "Crisler Arena", set: 2 },
-    { date: "1989-04-06", venue: "Crisler Arena", set: 1 },
-    { date: "1989-04-06", venue: "Crisler Arena", set: 2 },
-    { date: "1991-06-19", venue: "Pine Knob", set: 1 },
-    { date: "1991-06-19", venue: "Pine Knob", set: 2 },
-    { date: "1991-06-20", venue: "Pine Knob", set: 1 },
-    { date: "1991-06-20", venue: "Pine Knob", set: 2 },
-    { date: "1994-07-31", venue: "Palace of Auburn Hills", set: 1 },
-    { date: "1994-07-31", venue: "Palace of Auburn Hills", set: 2 },
-    { date: "1994-08-01", venue: "Palace of Auburn Hills", set: 1 },
-    { date: "1994-08-01", venue: "Palace of Auburn Hills", set: 2 }
-];
-
-
 
