54c2b20202e554ca1fb4a71bf1adc8beeeb7e15a
Bill Wagner <bill@thebillwagner.com>
2016-01-05 16:12:12 -0500

    Updates for Beta 0

--- DeadBaseSample/src/DeadBaseSample/TypeScriptSource/deadbase.component.ts
+++ DeadBaseSample/src/DeadBaseSample/TypeScriptSource/deadbase.component.ts
@@ -1,14 +1,12 @@
-﻿import { bootstrap, Component } from 'angular2/angular2';
+﻿import {  Component } from 'angular2/core';
 
 @Component({
     selector: 'deadbase-app',
     template: '<h1>{{title}}</h1><h2>{{venue}} Details!</h2>'
 })
-class DeadBaseAppComponent {
+export class DeadBaseAppComponent {
     public title = "Deadbase - Grateful Dead Concert Archive";
     public venue = "Filmore West";
 }
-
-bootstrap(DeadBaseAppComponent);
 
 
